db22cf587fc65b9df33941d2f58c2df5a3bc7ee8
Thetimelost <thetimesj@gmail.com>
2018-03-08 17:32:25 +0100

    Nettoyage + Correction de bug 08-03-18

---
+++ Unity_TFA_2018/Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour 
+{
+	public float TimeBeforeDead = 2.0f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		TimeBeforeDead -= Time.deltaTime;
+		if(TimeBeforeDead <= 0.0f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
