c43a67e20664b7a1f3e8068d0025f257afc301d8
thamex <thomas.voue@gmail.com>
2018-03-14 17:58:21 +0100

    Replace-Puzzle-Piege_AssetsPersoThomas

---
+++ AssetsPerso/Thomas/Piege+Puzzle/Piege-Puzzle_TFA_2018/Assets/Code/Script/Piege/TrapTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTrigger : MonoBehaviour 
+{
+	public GameObject BoxFixed;
+	public GameObject BoxExploded;
+	public ParticleSystem DustFall;
+	public ParticleSystem DustGround;
+	public int TrapDamage = 5;
+	public static int TrapDamageForScript;
+
+	public float TimerTrap = 1f;
+	// Use this for initialization
+	void Start () 
+	{
+		TrapDamageForScript = TrapDamage;
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+		{
+			StartCoroutine(TrapTimer(TimerTrap));
+		}	
+	}
+
+	IEnumerator TrapTimer(float timer)
+	{
+		DustFall.Play();
+		yield return new WaitForSeconds(timer);
+		BoxFixed.SetActive(false);
+		BoxExploded.SetActive(true);
+		yield return new WaitForSeconds(0.5f);
+		DustGround.Play();
+		Destroy(gameObject);
+	}
+
+	/*
+		faire decandre le fixed et switch au moment ou il tourche le joueur
+		faire un petit shake avant de tomber
+		faire vibrer la mannette au moment du DustFall Particle play
+		add un capsule collider surr le player pour pas que ça passe à travers lui	
+	 */
+}
---
+++ AssetsPerso/Thomas/Piege+Puzzle/Piege-Puzzle_TFA_2018/Assets/Code/Script/Puzzle/PuzzleTanvir.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTanvir : MonoBehaviour 
+{
+	public enum TypeOfPuzzle
+	{
+		DoorOpen,
+		PlateformUp
+	}
+	public TypeOfPuzzle TypePuz = TypeOfPuzzle.DoorOpen;
+
+	public GameObject Door;
+	public GameObject Plateform;
+
+	public GameObject First;
+	public GameObject Second;
+
+
+	public static bool OneOk = false;
+	public static bool SecOk = false;
+	
+	void Start () 
+	{
+
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	public void PullTrigger(Collider col)
+	{
+		if(OneOk && SecOk)
+		{
+			switch (TypePuz)
+			{
+				case TypeOfPuzzle.DoorOpen : 
+					Door.SetActive(false);
+					ImpulseForce.HaveTrigger = true;
+				break;
+				case TypeOfPuzzle.PlateformUp : 
+					Plateform.SetActive(true);
+					ImpulseForce.HaveTrigger = true;
+				break;
+				default:
+				break;
+			}
+		}
+	}
+}
