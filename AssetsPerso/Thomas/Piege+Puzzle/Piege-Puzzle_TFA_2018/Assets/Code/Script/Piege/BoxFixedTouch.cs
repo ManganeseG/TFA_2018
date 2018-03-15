5bad2968c122008f3cd29e26ee32cf3dbfd453bd
thamex <thomas.voue@gmail.com>
2018-03-15 11:19:26 +0100

    Assets-Perso_Thomas

---
+++ AssetsPerso/Thomas/Piege+Puzzle/Piege-Puzzle_TFA_2018/Assets/Code/Script/Piege/BoxFixedTouch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFixedTouch : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log("Touched");
+		TrapTrigger.BoxFixedHaveTouched = true;
+	}
+}
---
+++ AssetsPerso/Thomas/Piege+Puzzle/Piege-Puzzle_TFA_2018/Assets/Code/Script/Piege/PlayerTrapTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrapTest : MonoBehaviour 
+{
+	public int Health = 100;
+
+	void Update () 
+	{
+		Debug.Log(Health);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.layer == LayerMask.NameToLayer("Trap"))
+	        Health -= TrapTrigger.TrapDamageForScript;
+	}
+}
