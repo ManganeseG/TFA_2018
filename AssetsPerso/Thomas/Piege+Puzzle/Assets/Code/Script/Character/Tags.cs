0cd0e4f89a73ca9210020acb04fce17be72bace3
thamex <thomas.voue@gmail.com>
2018-05-03 12:32:42 +0200

    RemoveAssets

--- AssetsPerso/Thomas/Piege+Puzzle/Assets/Code/Script/Character/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tags : MonoBehaviour 
-{
-	// Liste des tags
-	public const string player = "Player";
-	public const string gameController = "GameController";
-}
